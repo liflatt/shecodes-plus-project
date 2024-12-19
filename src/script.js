043839586fc5151fb5e7e334ea09c15787911f81
liflatt <lflatt1221@gmail.com>
2024-12-19 15:44:23 -0600

    Added temperature unit toggle (Celsius/Farenheit)

--- src/script.js
+++ src/script.js
@@ -1,3 +1,8 @@
+let currentCity = "Fort Worth";
+let currentUnit = "imperial";
+let degreesUnit = "°F";
+let windUnit = "mph";
+
 function currentWeather(response) {
   let cityElement = document.querySelector("#current-city-display");
   let temperatureElement = document.querySelector(
@@ -11,10 +16,11 @@ function currentWeather(response) {
   let date = new Date(response.data.time * 1000);
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windElement.innerHTML = `${response.data.wind.speed}mph`;
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temp-emoji" />`;
+  windElement.innerHTML = `${response.data.wind.speed} ${windUnit}`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-forecast-icon" />`;
   timeElement.innerHTML = formatDate(date);
   cityElement.innerHTML = response.data.city;
 
@@ -31,7 +37,6 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-
   let months = [
     "Jan",
     "Feb",
@@ -59,9 +64,33 @@ function formatDate(date) {
   return `${day}, ${month} ${dayOfMonth}, ${hour}:${minutes}`;
 }
 
+function toggleTemperatureUnit() {
+  let toggleButton = document.getElementById("toggle-button");
+  let degreesElement = document.querySelector("#degrees");
+
+  if (currentUnit === "imperial") {
+    currentUnit = "metric";
+    degreesUnit = "°C";
+    windUnit = "km/h";
+    toggleButton.innerHTML = "°F";
+  } else {
+    currentUnit = "imperial";
+    degreesUnit = "°F";
+    windUnit = "mph";
+    toggleButton.innerHTML = "°C";
+  }
+
+  degreesElement.innerHTML = `${degreesUnit}`;
+  searchCity(currentCity);
+}
+
+let toggleButton = document.querySelector("#toggle-button");
+toggleButton.addEventListener("click", toggleTemperatureUnit);
+
 function searchCity(city) {
+  currentCity = city;
   let apiKey = "b0452f91cd75631eoba398t0f42a2100";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${currentUnit}`;
 
   axios.get(apiUrl).then(currentWeather);
 }
@@ -76,38 +105,35 @@ function handleSearch(event) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
   return days[date.getDay()];
 }
 
 function getForecast(city) {
   let apiKey = "b0452f91cd75631eoba398t0f42a2100";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
-  axios(apiUrl).then(showForecast);
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${currentUnit}`;
+
+  axios.get(apiUrl).then(displayForecast);
 }
 
-function showForecast(response) {
+function displayForecast(response) {
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
     if (index < 5) {
       forecastHtml =
         forecastHtml +
-        `
-      <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${formatDay(day.time)}</div>
-        
-        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
-        <div class="weather-forecast-temperatures">
-          <div class="weather-forecast-temperature">
-            <strong>${Math.round(day.temperature.maximum)}º</strong>
+        `<div class="weather-forecast-day">
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+          <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+              <strong>${Math.round(day.temperature.maximum)}º</strong>
+            </div>
+            <div class="weather-forecast-temperature">${Math.round(
+              day.temperature.minimum
+            )}º</div>
           </div>
-          <div class="weather-forecast-temperature">${Math.round(
-            day.temperature.minimum
-          )}º</div>
-        </div>
-      </div>
-    `;
+        </div>`;
     }
   });
 
